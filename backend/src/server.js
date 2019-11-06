0bdc51c9c653910d44115bc8bade334b97096e68
Lenivene Bezerra <lenivene@msn.com>
2019-11-06 19:16:26 -0300

    IntelliSense

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,42 +1,44 @@
-const express = require('express');
-const socketio = require('socket.io');
+const express = require("express");
+const socketio = require("socket.io");
 
-const cors = require('cors');
-const path = require('path');
-const http = require('http');
+const cors = require("cors");
+const path = require("path");
+const http = require("http");
 
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose');
-
-const routes = require('./routes');
+const routes = require("./routes");
 
 const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
-mongoose.connect('mongodb+srv://******:******@******.mongodb.net/AIRCNC?retryWrites=true&w=majority', {
-    useNewUrlParser : true,
-    useUnifiedTopology : true
-});
+mongoose.connect(
+  "mongodb+srv://******:******@******.mongodb.net/AIRCNC?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 var connectedUsers = {};
 
-io.on('connection', socket => {
-    const { user_id } = socket.handshake.query;
+io.on("connection", socket => {
+  const { user_id } = socket.handshake.query;
 
-    connectedUsers[ user_id ] = socket.id;
+  connectedUsers[user_id] = socket.id;
 });
 
-app.use( ( req, res, next ) => {
-    req.io = io;
-    req.connectedUsers = connectedUsers;
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
 
-    return next();
+  return next();
 });
 
-app.use( cors() );
-app.use( express.json() );
-app.use( '/files', express.static( path.resolve( __dirname, '..', 'uploads' ) ) );
-app.use( routes );
+app.use(cors());
+app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
+app.use(routes);
 
 server.listen(3333);
